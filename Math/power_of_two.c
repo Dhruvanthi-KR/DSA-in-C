f1397df18ba68b36dcf70aed613c8f5f323162f7
Dhruvanthi KR <dhruvanthikr153@gmail.com>
2026-02-27 19:03:06 +0530

    Added power of two soln

---
+++ Math/power_of_two.c
@@ -0,0 +1,11 @@
+bool isPowerOfTwo(int n) {
+    if(n <= 0){
+        return false;
+    }
+    for(int i = 0 ; i <= 31; i++){
+        if(pow(2,i) == n){
+            return true;
+        }
+    }
+    return false;
+}
