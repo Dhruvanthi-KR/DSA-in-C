5156095a8f0d1688d6e390a41bf359803a9b0cd6
Dhruvanthi KR <dhruvanthikr153@gmail.com>
2026-02-27 19:05:04 +0530

    Added power of four soln

---
+++ Math/power_of_four.c
@@ -0,0 +1,10 @@
+bool isPowerOfFour(int n) {
+    if(n <= 0){
+        return false;
+    }
+    for(int i = 0; i <= 31;i++){
+        if(pow(4,i) == n){
+            return true;
+        }
+    }return false;
+}
