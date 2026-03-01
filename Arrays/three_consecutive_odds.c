ade35510c13f1b7bef59e2ddcad1a3aa0becaf0e
Dhruvanthi KR <dhruvanthikr153@gmail.com>
2026-03-01 12:29:34 +0530

    Added three consecutive odds soln

---
+++ Arrays/three_consecutive_odds.c
@@ -0,0 +1,13 @@
+bool threeConsecutiveOdds(int* arr, int arrSize) {
+    int count = 0, streak = 0;
+    for(int i = 0; i < arrSize; i++){
+        if(arr[i] % 2 != 0){
+            count++;
+            if(count == 3){
+                return true;
+            }
+        }else{
+            count = 0;
+        }
+    }return 0;
+}
