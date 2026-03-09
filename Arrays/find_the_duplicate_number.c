2a7596fd473f7deb966cca9eba1ff5a7a6522d71
Dhruvanthi KR <dhruvanthikr153@gmail.com>
2026-03-09 20:16:21 +0530

    Added find the duplicate number soln

---
+++ Arrays/find_the_duplicate_number.c
@@ -0,0 +1,12 @@
+int findDuplicate(int* nums, int numsSize) {
+    int visit[numsSize + 1];
+    for(int i = 0; i < numsSize;i++){
+        visit[i] = 0;
+    }
+    for(int i = 0; i < numsSize; i++){
+        if(visit[nums[i]] == 1){
+            return nums[i];
+        }visit[nums[i]] = 1;
+    }
+    return -1;
+}
