af50d0226b2bc3895797a06e1f7901f2bcd4e79a
Dhruvanthi KR <dhruvanthikr153@gmail.com>
2026-03-09 21:46:15 +0530

    Add problem details Concatenation of Array

--- Arrays/concatenation_of_array.c
+++ Arrays/concatenation_of_array.c
@@ -1,6 +1,12 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+/*
+Problem No: 1929
+Problem: Concatenation of Array
+Platform: LeetCode
+Topic: Arrays
+Approach: Create a new array of size 2*n and copy nums twice
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 int* getConcatenation(int* nums, int numsSize, int* returnSize) {
     int dou = numsSize + numsSize;
     int *retArr = (int*)malloc(dou*sizeof(int));
